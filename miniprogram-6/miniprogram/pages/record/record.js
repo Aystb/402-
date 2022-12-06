244cb14b2f4c4ff8812267c3be9f561368f74ad3
hiraeth <113422546+hiraethsp@users.noreply.github.com>
2022-12-06 15:38:47 +0800

    修改一处错误

--- miniprogram-6/miniprogram/pages/record/record.js
+++ miniprogram-6/miniprogram/pages/record/record.js
@@ -9,7 +9,7 @@ Page({
       day: "2022-12-10",
       hour: "10:00-11:00",
       text: "此处为预约理由"
-    },{
+    }, {
       day: "2022-12-10",
       hour: "10:00-11:00",
       text: "此处为预约理由"
@@ -33,31 +33,31 @@ Page({
   },
   Get() {
     wx.request({
-      url: 'http://127.0.0.1:8000/users/' + app.globalData.id + '/records',
-      method: 'Get',
-    
-      success: (res) => {
-        const time = new Date();
-        const today = time.getDate();
-        const currentMonth = time.getMonth() + 1;
-        const currentHour = time.getHours();
-        const currentYear = time.getFullYear();
-        console.log(today);
-        console.log(currentMonth);
-        console.log(currentHour);
-        console.log(currentYear);
-        for (let data of res.data) {
-          const thisStartTime = new Date(data.starttime);
-          const thisEndTime = new Data(data.endtime);
-          const thisMonth = thisStartTime.getMonth() + 1;
-          const thisDay = thisStartTime.getDate();
-          const thisStartHour = thisStartTime.getHours();
-          const thisEndHour = thisEndTime.getHours();
-          const thisYear = thisStartTime.getFullYear();
-          var d1 = new Date(time.replace(/\-/g, "\/"));
-          var d2 = new Date(thisEndTime.replace(/\-/g, "\/"));
+        url: 'http://127.0.0.1:8000/users/' + app.globalData.id + '/records',
+        method: 'Get',
 
-          if (d1 < d2 && currentHour < thisEndHour) {
+        success: (res) => {
+          const time = new Date();
+          const today = time.getDate();
+          const currentMonth = time.getMonth() + 1;
+          const currentHour = time.getHours();
+          const currentYear = time.getFullYear();
+          console.log(today);
+          console.log(currentMonth);
+          console.log(currentHour);
+          console.log(currentYear);
+          for (let data of res.data) {
+            const thisStartTime = new Date(data.starttime);
+            const thisEndTime = new Data(data.endtime);
+            const thisMonth = thisStartTime.getMonth() + 1;
+            const thisDay = thisStartTime.getDate();
+            const thisStartHour = thisStartTime.getHours();
+            const thisEndHour = thisEndTime.getHours();
+            const thisYear = thisStartTime.getFullYear();
+            var d1 = new Date(time.replace(/\-/g, "\/"));
+            var d2 = new Date(thisEndTime.replace(/\-/g, "\/"));
+
+            if (d1 < d2 || (d1 == d2 && currentHour < thisEndHour) ) {
             const thisTime = {
               day: thisYear + "-" + thisMonth + "-" + thisDay,
               hour: thisStartHour + ":00-" + thisEndHour + ":00",
@@ -70,53 +70,53 @@ Page({
         }
       },
     })
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
+},
+/**
+ * 生命周期函数--监听页面加载
+ */
 
-  moreRecord() {
-    wx.navigateTo({
-      url: '/pages/moreRecord/moreRecord'
-    });
-  },
-  onLoad() {
-    this.loadFontFace();
-    this.Get()
-  },
+moreRecord() {
+  wx.navigateTo({
+    url: '/pages/moreRecord/moreRecord'
+  });
+},
+onLoad() {
+  this.loadFontFace();
+  this.Get()
+},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {},
+/**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+onReady() {},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {},
+/**
+ * 生命周期函数--监听页面显示
+ */
+onShow() {},
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {},
+/**
+ * 生命周期函数--监听页面隐藏
+ */
+onHide() {},
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {},
+/**
+ * 生命周期函数--监听页面卸载
+ */
+onUnload() {},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {},
+/**
+ * 页面相关事件处理函数--监听用户下拉动作
+ */
+onPullDownRefresh() {},
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {},
+/**
+ * 页面上拉触底事件的处理函数
+ */
+onReachBottom() {},
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {},
+/**
+ * 用户点击右上角分享
+ */
+onShareAppMessage() {},
 });
