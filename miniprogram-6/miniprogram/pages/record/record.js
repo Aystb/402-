a214023c174b690610ffb8d66d2da45d2a6dc8d2
hiraeth <113422546+hiraethsp@users.noreply.github.com>
2022-12-11 17:32:59 +0800

    时间判断修改

--- miniprogram-6/miniprogram/pages/record/record.js
+++ miniprogram-6/miniprogram/pages/record/record.js
@@ -5,15 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list: [{
-      day: "2022-12-10",
-      hour: "10:00-11:00",
-      text: "此处为预约理由"
-    }, {
-      day: "2022-12-10",
-      hour: "10:00-11:00",
-      text: "此处为预约理由"
-    }]
+    list: []
   },
 
   loadFontFace() {
@@ -33,35 +25,32 @@ Page({
   },
   Get() {
     wx.request({
-        url: 'http://127.0.0.1:8000/users/' + app.globalData.id + '/records',
-        method: 'Get',
 
-        success: (res) => {
-          const time = new Date();
-          const today = time.getDate();
-          const currentMonth = time.getMonth() + 1;
-          const currentHour = time.getHours();
-          const currentYear = time.getFullYear();
-          console.log(today);
-          console.log(currentMonth);
-          console.log(currentHour);
-          console.log(currentYear);
-          for (let data of res.data) {
-            const thisStartTime = new Date(data.starttime);
-            const thisEndTime = new Data(data.endtime);
-            const thisMonth = thisStartTime.getMonth() + 1;
-            const thisDay = thisStartTime.getDate();
-            const thisStartHour = thisStartTime.getHours();
-            const thisEndHour = thisEndTime.getHours();
-            const thisYear = thisStartTime.getFullYear();
-            var d1 = new Date(time.replace(/\-/g, "\/"));
-            var d2 = new Date(thisEndTime.replace(/\-/g, "\/"));
+      url: 'https://api.room402.temp.ziqiang.net.cn/api/1.0/users/' + app.globalData.id + '/records',
+      method: 'Get',
 
-            if (d1 < d2 || (d1 == d2 && currentHour < thisEndHour) ) {
+      success: (res) => {
+        console.log(res.data)
+        const time = new Date();
+        const today = time.getDate();
+        const currentMonth = time.getMonth() + 1;
+        const currentHour = time.getHours();
+        const currentYear = time.getFullYear();
+        for (let data of res.data) {
+          const thisStartTime = new Date(data.starttime);
+          // console.log("start:"+thisStartTime);
+          const thisEndTime = new Date(data.endtime);
+          const thisMonth = thisStartTime.getMonth() + 1;
+          const thisDay = thisStartTime.getDate();
+          // console.log("thisDay:"+thisDay)
+          const thisStartHour = thisStartTime.getHours();
+          const thisEndHour = thisEndTime.getHours();
+          const thisYear = thisStartTime.getFullYear();
+          if (thisEndTime>time) {
             const thisTime = {
               day: thisYear + "-" + thisMonth + "-" + thisDay,
               hour: thisStartHour + ":00-" + thisEndHour + ":00",
-              text: res.data.choice + "\n" + res.data.text
+              text: data.choice + "\n" + data.text
             }
             this.setData({
               list: this.data.list.concat(thisTime)
@@ -70,53 +59,53 @@ Page({
         }
       },
     })
-},
-/**
- * 生命周期函数--监听页面加载
- */
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
 
-moreRecord() {
-  wx.navigateTo({
-    url: '/pages/moreRecord/moreRecord'
-  });
-},
-onLoad() {
-  this.loadFontFace();
-  this.Get()
-},
+  moreRecord() {
+    wx.navigateTo({
+      url: '/pages/moreRecord/moreRecord'
+    });
+  },
+  onLoad() {
+    this.loadFontFace();
+    this.Get()
+  },
 
-/**
- * 生命周期函数--监听页面初次渲染完成
- */
-onReady() {},
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
 
-/**
- * 生命周期函数--监听页面显示
- */
-onShow() {},
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
 
-/**
- * 生命周期函数--监听页面隐藏
- */
-onHide() {},
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
 
-/**
- * 生命周期函数--监听页面卸载
- */
-onUnload() {},
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
 
-/**
- * 页面相关事件处理函数--监听用户下拉动作
- */
-onPullDownRefresh() {},
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
 
-/**
- * 页面上拉触底事件的处理函数
- */
-onReachBottom() {},
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
 
-/**
- * 用户点击右上角分享
- */
-onShareAppMessage() {},
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
 });
